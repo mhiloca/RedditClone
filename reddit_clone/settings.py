aee8f19d02534ced0d9ed55a315d7aec0506e470
Mhirley Lopes <mhiloca@gmail.com>
2019-10-08 17:23:34 -0300

    set database, created .gitignore, created .env
    created templates directory

--- reddit_clone/settings.py
+++ reddit_clone/settings.py
@@ -55,7 +55,7 @@ ROOT_URLCONF = 'reddit_clone.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR, 'templates'),],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
