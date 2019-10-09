d23efe09462ddfbcd8787e30d4d696a10fdd5635
Mhirley Lopes <mhiloca@gmail.com>
2019-10-09 13:16:31 -0300

    fixed dashboard bug

--- accounts/urls.py
+++ accounts/urls.py
@@ -7,5 +7,5 @@ urlpatterns = [
     path('signup/', sign_up, name='signup'),
     path('login/', log_in, name='login'),
     path('logout/', auth_views.LogoutView.as_view(), name='logout'),
-    path('dashboard/', dashboard, name='dashboard')
+    path('dashboard/', dashboard, name='dashboard'),
 ]
